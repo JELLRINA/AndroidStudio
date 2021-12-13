e8ab76df0835940d3d0b741e0180a6110850c524
jellrina <dlgh6007@gmail.com>
2021-12-14 00:58:08 +0900

    자동완성텍스트뷰

--- app/src/main/java/com/example/multiautocompletetextview/MainActivity.java
+++ app/src/main/java/com/example/multiautocompletetextview/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.multiautocompletetextview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.MultiAutoCompleteTextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] items = {"부산광역시", "서울특별시", "대구광역시","서구", "중구", "남구"};
+
+        AutoCompleteTextView auto = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line,items);
+        auto.setAdapter(adapter);
+        MultiAutoCompleteTextView multi = (MultiAutoCompleteTextView) findViewById(R.id.multiAutoCompleteTextView);
+        MultiAutoCompleteTextView.CommaTokenizer token = new MultiAutoCompleteTextView.CommaTokenizer();
+        multi.setTokenizer(token);
+        multi.setAdapter(adapter);
     }
 }
